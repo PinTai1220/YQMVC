2646d8e93bd2fa0e3d03eba2a6c55ccfcc327ffb
WarlockJobs <998522+WarlockJobs@users.noreply.github.com>
2019-04-13 08:40:23 +0800

    Api加密

--- YQMVC/Helper/HttpClientHelper.cs
+++ YQMVC/Helper/HttpClientHelper.cs
@@ -10,13 +10,16 @@ namespace YQMVC.Helper
 {
     public class HttpClientHelper
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="url"></param>
-        /// <param name="method"></param>
-        /// <param name="data">json字符串格式数据</param>
-        /// <returns></returns>
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="url">访问的路径</param>
+       /// <param name="method">访问的方法</param>
+       /// <param name="timestamp">时间戳</param>
+       /// <param name="nonce">随机数</param>
+       /// <param name="signature">公钥</param>
+       /// <param name="data">传输的数据</param>
+       /// <returns></returns>
         public static string SendRequest(string url, string method, string timestamp, string nonce, string signature, string data = "")
         {
             HttpClient client = new HttpClient();
