e59a9b516e173dd763dd39358c211e1bee95a544
bitType <2386690386@qq.com>
2019-04-18 16:22:37 +0800

    5454

--- YQMVC/Controllers/OrderController.cs
+++ YQMVC/Controllers/OrderController.cs
@@ -19,22 +19,57 @@ namespace YQMVC.Controllers
         [LoginAuthorization]
         public ActionResult Show()
         {
+            return View();
+        }
+        [HttpGet]
+        [LoginAuthorization]
+        public dynamic Data(int page, int limit)
+        {
             Dictionary<string, string> dic = new Dictionary<string, string>();
             dic.Add("", "");
-            
+
             string nonce = DataTransfer.GetNonce().ToString();
             string timestamp = DataTransfer.GetTimeStamp();
             string signature = DataTransfer.GetMD5Staff(dic, timestamp, nonce);
 
             string result = HttpClientHelper.SendRequest("api/Orders/Show", "get", timestamp, nonce, signature, "");
             List<Orders> list = JsonConvert.DeserializeObject<List<Orders>>(result);
-            return View(list);
+            var data = new
+            {
+                code = 0,
+                msg = "",
+                count = list.Count(),
+                data = list
+            };
+            return JsonConvert.SerializeObject(data);
+        }
+        [HttpPost]
+        [LoginAuthorization]
+        public string Upt(int uid, string name, string sex, string cid)
+        {
+            UserInfos u = new UserInfos()
+            {
+                UserInfo_Id = uid,
+                UserInfo_Name = name,
+                UserInfo_Sex = sex == "男" ? 0 : 1,
+                ID_Num = cid
+            };
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("UserInfo_Id", uid.ToString());
+            dic.Add("UserInfo_Name", name);
+            dic.Add("UserInfo_Sex", sex == "男" ? "0" : "1");
+            dic.Add("ID_Num", cid);
+            dic.Add("Phone_Num", "");
+            dic.Add("Address", "");
+            dic.Add("HeadImg", "");
+
+
+            string nonce = DataTransfer.GetNonce().ToString();
+            string timestamp = DataTransfer.GetTimeStamp();
+            string signature = DataTransfer.GetMD5Staff(dic, timestamp, nonce);
+            string result = HttpClientHelper.SendRequest("api/Orders/Show", "post", timestamp, nonce, signature, JsonConvert.SerializeObject(u));
+            return result;
         }
     }
-    public enum State
-    {
-        已预定=0,
-        已入住=1,
-        空房=3       
-    }
+   
 }
