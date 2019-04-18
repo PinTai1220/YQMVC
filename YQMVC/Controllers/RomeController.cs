62fdc09337a7f54ec74d5a91b5b605be08051546
bitType <2386690386@qq.com>
2019-04-18 14:23:44 +0800

    6565

---
+++ YQMVC/Controllers/RomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YQMVC.Helper;
+using YQMVC.Models;
+using Newtonsoft.Json;
+
+
+namespace YQMVC.Controllers
+{
+    public class RomeController : Controller
+    {
+        public ActionResult Show()
+        {
+            return View();
+        }
+        [HttpGet]
+        public dynamic Get(string sstate,int page,int limit)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("", "");
+
+            string nonce = DataTransfer.GetNonce().ToString();
+            string timestamp = DataTransfer.GetTimeStamp();
+            string signature = DataTransfer.GetMD5Staff(dic, timestamp, nonce);
+
+            string result = HttpClientHelper.SendRequest("api/rooms/Show", "get", timestamp, nonce, signature, "");
+            List<Rooms> list = JsonConvert.DeserializeObject<List<Rooms>>(result);
+            var da = (from a in list
+                     where a.RoomType_Name == sstate
+                     select a).Skip(page*limit).Take(limit).ToList();
+
+            var data = new
+            {
+                code = 0,
+                msg = "",
+                count = list.Count(),
+                data = list
+            };
+            return JsonConvert.SerializeObject(data);
+        }
+    }
+}
--- YQMVC/Models/Rooms.cs
+++ YQMVC/Models/Rooms.cs
@@ -14,5 +14,7 @@ namespace YQMVC.Models
         public int RoomType_Id { get; set; }//类型编号
         public int Room_Num { get; set; }//房间号
         public int Room_Type { get; set; }//房间状态
+        public string RoomType_Name { get; set; }//房间类型名称
+        public decimal RoomType_Price { get; set; }//房间价格
     }
 }
