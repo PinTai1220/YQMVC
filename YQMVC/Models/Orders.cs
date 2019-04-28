a0f09bd38af16549681c1e2cf71025f209542396
bitType <2386690386@qq.com>
2019-04-28 09:46:26 +0800

    44444

--- YQMVC/Models/Orders.cs
+++ YQMVC/Models/Orders.cs
@@ -17,7 +17,7 @@ namespace YQMVC.Models
         public decimal Money_Favorable { get; set; }//优惠金额
         public decimal Money_Should { get; set; }//应付金额
         public string Type_Of_Payment { get; set; }//支付类型
-        public DateTime Checkln_Time { get; set; }//入住时间
+        public DateTime CheckIn_Time { get; set; }//入住时间
         public DateTime Leave_Time { get; set; }//退房时间
         public int Room_State { get; set; }//房间编号
         public string Phone_Num { get; set; }
