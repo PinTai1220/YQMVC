cb963fd8331eb916734e3bbf8cfd50904e204a2a
2941451132@qq.com <2941451132@qq.com>
2019-04-28 10:30:38 +0800

    da

--- YQMVC/Controllers/UserInfoController.cs
+++ YQMVC/Controllers/UserInfoController.cs
@@ -15,9 +15,15 @@ namespace YQMVC.Controllers
     /// </summary>
     public class UserInfoController : Controller
     {
-        // GET: UserInfo
         [LoginAuthorization]
         public ActionResult UserInfoIndex()
+        {
+            return View();
+        }
+        // GET: UserInfo
+        [HttpGet]
+        [LoginAuthorization]
+        public dynamic UserInfoGet(int page, int limit)
         {
             Dictionary<string, string> dic = new Dictionary<string, string>();
             dic.Add("", "");
@@ -28,7 +34,21 @@ namespace YQMVC.Controllers
 
             string result = HttpClientHelper.SendRequest("api/UserInfos/Show", "get", timestamp, nonce, signature, "");
             List<UserInfos> userinfos = JsonConvert.DeserializeObject<List<UserInfos>>(result);
-            return View(userinfos);
+            var data = new
+            {
+                code = 0,
+                msg = "",
+                count = userinfos.Count(),
+                data = from a in userinfos
+                       select new
+                       {
+                           UserInfo_Name = a.UserInfo_Name,
+                           Sex = a.UserInfo_Sex == 0 ? "男" : "女",
+                           ID_Num = a.ID_Num,
+                           Phone_Num = a.Phone_Num
+                       }
+            };
+            return JsonConvert.SerializeObject(data);
         }
     }
 }
